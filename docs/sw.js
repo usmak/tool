e3252e4df27f80fb73c5d559e65cc181c83f1983
usmak <pixgallery83@gmail.com>
2026-06-14 02:44:26 +0530

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'toolbox-v1'
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(['./', './index.html']))
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  )
+})
